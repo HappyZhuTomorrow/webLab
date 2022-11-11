71a7ccdde38408ec07a039d47a6a0f18b783effc
Alcremie <2282931136@qq.com>
2022-11-11 13:34:54 +0800

    电影爬虫完成

--- foo/webCrawler.py
+++ foo/webCrawler.py
@@ -1,19 +1,120 @@
+from unittest import result
 import requests
 import time
+import os
+from bs4 import BeautifulSoup
+import json
+import re
+from lxml import etree
 
 urlMovie = 'https://movie.douban.com/subject/{}/'
 urlBook = 'https://book.douban.com/subject/{}/'
 
 def getMovieURL():
-    # with open('1.txt','r') as movieFile:
-    #     print(movieFile.readline())
-    file = open('1.txt','r',encoding='utf-8')
-    print(file.readline())
-    file.close()
-def getBookURL():
-    pass
+    with open('../docs/Movie_id.txt','r') as movieFile:
+        print(movieFile.readline())
+        lines = movieFile.readlines()
+        lines = map(lambda x:x.strip(),lines)
+        URLs = map(lambda x:urlMovie.format(x),lines)
+        return list(URLs)
 
+def getBookURL():
+    with open('../docs/Book_id.txt','r') as bookFile:
+        print(bookFile.readline())
+        lines = bookFile.readlines()
+        lines = map(lambda x:x.strip(),lines)
+        URLs = map(lambda x:urlMovie.format(x),lines)
+        return list(URLs)
+
+def doubanMovie(url):
+    head = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    }
+    r = requests.get(url,headers=head)
+    return r.text
+    result = {
+        "电影名:":[],
+        "导演:":[],
+        "编剧:":[],
+        "主演:":[],
+        "类型:":[],
+        "制片国家/地区:":[],
+        "语言:":[],
+        "上映日期:":[],
+        "片长:":[],
+        "又名:":[],
+        "IMDb:":[]
+    }
+    # return r
+    # soup = BeautifulSoup(r.text,features='lxml')
+    # print(soup.prettify())    
+    # movieName = soup.h1.span.string
+    # print(soup.select('#info > span')[1])
+    s = etree.HTML(r.text)
+    # print('电影名:',s.xpath('//*[@id="content"]/h1/span[1]/text()'))
+    # print('导演:',s.xpath('//*[@id="info"]/span[1]/span[2]/a/text()'))
+    # print('编剧:',s.xpath('//*[@id="info"]/span[2]/span[2]/a/text()'))
+    # print('主演:',s.xpath('//*[@id="info"]/span[3]/span[2]/a/text()'))
+    result['导演:'] = s.xpath('//*[@id="content"]/h1/span[1]/text()')
+    result['编剧:'] = s.xpath('//*[@id="info"]/span[2]/span[2]/a/text()')
+    result['主演:'] = s.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
+    result['电影名:'] = s.xpath('//*[@id="content"]/h1/span[1]/text()')
+    soup = BeautifulSoup(r.text,features='lxml')
+    datas = str(soup.find_all(id='info')).split('\n')
+    del datas[0:4]
+    del datas[-1]
+    for data in datas:
+        reResult = re.findall('>(.*?)</span>',data)
+        for _r in reResult[1:]:
+            result[reResult[0]].append(_r)
+
+        r1 = re.sub('>(.*?)</span>','',data)
+        r2 = re.findall('<span class="pl"(.*?)<br/>',r1)
+        if(result[reResult[0]] == []):
+            result[reResult[0]].append(r2[0].strip(' '))
+
+    return result
+    
+
+def handleMovieData(text):
+    result = {
+        "电影名:":[],
+        "导演:":[],
+        "编剧:":[],
+        "主演:":[],
+        "类型:":[],
+        "制片国家/地区:":[],
+        "语言:":[],
+        "上映日期:":[],
+        "片长:":[],
+        "又名:":[],
+        "IMDb:":[]
+    }
+    s = etree.HTML(text)
+    result['导演:'] = s.xpath('//*[@id="info"]/span[1]/span[2]/a/text()')
+    result['编剧:'] = s.xpath('//*[@id="info"]/span[2]/span[2]/a/text()')
+    result['主演:'] = s.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
+    result['电影名:'] = s.xpath('//*[@id="content"]/h1/span[1]/text()')
+
+    soup = BeautifulSoup(text,features='lxml')
+    datas = str(soup.find_all(id='info')).split('\n')
+    del datas[0:4]
+    del datas[-1]
+    for data in datas:
+        reResult = re.findall('>(.*?)</span>',data)
+        for _r in reResult[1:]:
+            result[reResult[0]].append(_r)
+
+        r1 = re.sub('>(.*?)</span>','',data)
+        r2 = re.findall('<span class="pl"(.*?)<br/>',r1)
+        if(result[reResult[0]] == []):
+            result[reResult[0]].append(r2[0].strip(' '))
+
+    return result
 
 
 if __name__ == '__main__':
-    getMovieURL()
+    # print(getMovieURL())
+    responseText = doubanMovie('https://movie.douban.com/subject/1292052/')
+    result = handleMovieData(responseText)
+    print(result)
