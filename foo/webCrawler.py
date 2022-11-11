37e49c85042fda8641e0da21d021cda6d0270fcf
Alcremie <2282931136@qq.com>
2022-11-12 00:45:55 +0800

    电影和图书爬取完毕

--- foo/webCrawler.py
+++ foo/webCrawler.py
@@ -1,8 +1,4 @@
-from unittest import result
-from flask import flash
-from matplotlib.pyplot import flag
-from numpy import result_type
-from pyparsing import null_debug_action
+
 import requests
 import time
 import os
@@ -10,6 +6,7 @@ from bs4 import BeautifulSoup
 import json
 import re
 from lxml import etree
+from yarl import URL
 
 urlMovie = 'https://movie.douban.com/subject/{}/'
 urlBook = 'https://book.douban.com/subject/{}/'
@@ -93,62 +90,67 @@ def handleMovieData(text):
         "剧情简介":{},
         "演职员表":{}
     }
-    s = etree.HTML(text)
-    result["基本信息"]['电影名:'] = s.xpath('//*[@id="content"]/h1/span[1]/text()')[0]
-    result["基本信息"]['导演:'] = s.xpath('//*[@id="info"]/span[1]/span[2]/a/text()')
-    result["基本信息"]['编剧:'] = s.xpath('//*[@id="info"]/span[2]/span[2]/a/text()')
-    result["基本信息"]['主演:'] = s.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
-    
-    # print(s.xpath('//*[@id="link-report-intra"]/span[2]/text()'))
-
-    soup = BeautifulSoup(text,features='lxml')
-    
-    datas = str(soup.find_all(id='info')).split('\n')
-    del datas[0:4]
-    del datas[-1]
-    for data in datas:
-        try:
-            reResult = re.findall('>(.*?)</span>',data)
-            result["基本信息"][reResult[0]] = reResult[1:]
-
-            r1 = re.sub('>(.*?)</span>','',data)
-            r2 = re.findall('<span class="pl"(.*?)<br/>',r1)
-            if(result["基本信息"][reResult[0]] == []):
-                result["基本信息"][reResult[0]].append(r2[0].strip(' '))
-        except:
-            pass
-
+    try:
+        s = etree.HTML(text)
+        result["基本信息"]['电影名:'] = s.xpath('//*[@id="content"]/h1/span[1]/text()')[0]
+        result["基本信息"]['导演:'] = s.xpath('//*[@id="info"]/span[1]/span[2]/a/text()')
+        result["基本信息"]['编剧:'] = s.xpath('//*[@id="info"]/span[2]/span[2]/a/text()')
+        result["基本信息"]['主演:'] = s.xpath('//*[@id="info"]/span[3]/span[2]/a/text()')
         
-    # intro = str(soup.select('span[class="all hidden"]')[0])
-    # intro = intro.replace(' ','').replace('<br/>','').replace('\n','').replace(chr(12288),'')
-    # reResult = re.findall('<spanclass="allhidden">(.*?)</span>',intro)
-    a = str(soup.select('div[class="related-info"]'))
-    if(re.findall('剧情简介',a)):
-        if(re.findall('all hidden',a)):
-            intro = soup.select('div[class="indent"] > span[class="all hidden"]')[0].get_text().strip().replace('\n','')
-            intro = ''.join(intro.split())
-        else:
-            intro = soup.select('div[class="indent"] > span')[0].get_text().strip().replace('\n','')
-            intro = ''.join(intro.split())
-            # print(intro)
-        result['剧情简介'] = intro
+        # print(s.xpath('//*[@id="link-report-intra"]/span[2]/text()'))
+
+        soup = BeautifulSoup(text,features='lxml')
+        
+        datas = str(soup.find_all(id='info')).split('\n')
+        del datas[0:4]
+        del datas[-1]
+        for data in datas:
+            try:
+                reResult = re.findall('>(.*?)</span>',data)
+                result["基本信息"][reResult[0]] = reResult[1:]
+
+                r1 = re.sub('>(.*?)</span>','',data)
+                r2 = re.findall('<span class="pl"(.*?)<br/>',r1)
+                if(result["基本信息"][reResult[0]] == []):
+                    result["基本信息"][reResult[0]].append(r2[0].strip(' '))
+            except:
+                pass
+
+            
+        # intro = str(soup.select('span[class="all hidden"]')[0])
+        # intro = intro.replace(' ','').replace('<br/>','').replace('\n','').replace(chr(12288),'')
+        # reResult = re.findall('<spanclass="allhidden">(.*?)</span>',intro)
+        a = str(soup.select('div[class="related-info"]'))
+        if(re.findall('剧情简介',a)):
+            if(re.findall('all hidden',a)):
+                intro = soup.select('div[class="indent"] > span[class="all hidden"]')[0].get_text().strip().replace('\n','')
+                intro = ''.join(intro.split())
+            else:
+                intro = soup.select('div[class="indent"] > span')[0].get_text().strip().replace('\n','')
+                intro = ''.join(intro.split())
+                # print(intro)
+            result['剧情简介'] = intro
 
 
-    actors = soup.select('div[class="info"]')
-    actList = []
-    for i in actors:
-        act = {
-            "actor":"",
-            "character":""
-        }
-        i = str(i)
-        reS = re.findall('title="(.*)">',i)
-        # print(reS[0],'========>',reS[1])
-        act['actor'] = reS[0]
-        act['character'] = reS[1]
-        actList.append(act)
+        actors = soup.select('div[class="info"]')
+        actList = []
+        for i in actors:
+            act = {
+                "actor":"",
+                "character":""
+            }
+            i = str(i)
+            reS = re.findall('title="(.*)">',i)
+            # print(reS[0],'========>',reS[1])
+            act['actor'] = reS[0]
+            act['character'] = reS[1]
+            actList.append(act)
 
-    result['演职员表']['data'] = actList
+        result['演职员表']['data'] = actList
+    except:
+        print("不存在哦")
+        pass
+
     return result
 
 
@@ -170,49 +172,84 @@ def handleBook(text):
         "作者简介":{}
     }
 
+    try:
+        s = etree.HTML(text)
+        soup = BeautifulSoup(text,features='lxml')
+        
+        
+        
+        result['基本信息']['书名'] = s.xpath('//*[@id="wrapper"]/h1/span/text()')[0]
 
-    s = etree.HTML(text)
-    soup = BeautifulSoup(text,features='lxml')
-    
-    
-    
-    result['基本信息']['书名'] = s.xpath('//*[@id="wrapper"]/h1/span/text()')[0]
+        info = soup.select('div[id="info"]')
+        flag = 0
+        lastKey = ""
+        for i in info[0].contents:
+            a = i.get_text().strip()
+            a = re.sub('\\s|\n','',a)
+            if(a != ""):
+                # print('a[-1]',a[-1])
+                try:
+                    if(a[-1] != ':' and flag == 0):
+                        b = a.split(":")
+                        
+                        result['基本信息'][b[0]] = b[1]
+                    else:
+                        if(flag == 0):
+                            result['基本信息'][a] = ""
+                            lastKey = a
+                        else:
+                            result['基本信息'][lastKey] = a
 
-    info = soup.select('div[id="info"]')
-    flag = 0
-    lastKey = ""
-    for i in info[0].contents:
-        a = i.get_text().strip()
-        a = re.sub('\\s|\n','',a)
-        if(a != ""):
-            # print('a[-1]',a[-1])
-            if(a[-1] != ':' and flag == 0):
-                b = a.split(":")
-                result['基本信息'][b[0]] = b[1]
-            else:
-                if(flag == 0):
-                    result['基本信息'][a] = ""
-                    lastKey = a
+                        flag = (flag + 1) % 2
+                except:
+                    pass
+                    
+
+        i = 0
+        for sibling in soup.h2.next_siblings:
+            if(i==1):
+                if(re.findall('all hidden',str(sibling))):
+                    # print(sibling.span.next_sibling.next_sibling.get_text().strip())
+                    result['内容简介']['intro'] = sibling.span.next_sibling.next_sibling.get_text().strip()
                 else:
-                    result['基本信息'][lastKey] = a
+                    # print(sibling.get_text().strip())
+                    result['内容简介']['intro'] = sibling.get_text().strip()
+            if(i==7):
+                if(re.findall('all hidden',str(sibling))):
+                    result['作者简介']['intro'] = sibling.span.next_sibling.next_sibling.get_text().strip()
+                    # print(sibling.span.next_sibling.next_sibling.get_text().strip())
+                else:
+                    result['作者简介']['intro'] = sibling.get_text().strip()
+                    # print(sibling.get_text().strip())
+                break
+            i += 1
+    except:
+        pass    
 
-                flag = (flag + 1) % 2
-                
+        
 
-    
 
-    a = str(soup.select('div[class="related_info"]'))
-    if(re.findall('内容简介',a)):
-        if(re.findall('all hidden',a)):
-            contentIntro = soup.select('span[class="all hidden"] > div > div[class="intro"] > p')[0].get_text()
-            # print(contentIntro)
-        else:
-            contentIntro = soup.select('div[class="indent"] > div > div[class="intro"] > p')[0].get_text()
-            # print(contentIntro)
-        result['内容简介']['intro'] = contentIntro
-    if(re.findall('作者简介',a)):
-        authorIntro = soup.select('div[class="indent"] > div > div[class="intro"]')[0].get_text().strip()
-        result['作者简介']['intro'] = authorIntro
+        # if(i == 1):
+        #     print(sibling.get_text())
+        # i = (i+1)%2
+
+    # a = str(soup.select('div[class="related_info"]'))
+    # if(re.findall('内容简介',a)):
+    #     if(re.findall('all hidden',a)):
+    #         try:
+    #             contentIntro = soup.select('span[class="all hidden"] > div > div[class="intro"] > p')[0].get_text()
+    #         except:
+    #             contentIntro = soup.select('div[class="indent"] > div > div[class="intro"] > p')[0].get_text()
+    #     else:
+    #         contentIntro = soup.select('div[class="indent"] > div > div[class="intro"] > p')[0].get_text()
+    #     result['内容简介']['intro'] = contentIntro
+    # if(re.findall('作者简介',a)):
+    #     if(re.findall('all hidden',a)):
+    #         try:
+    #             authorIntro = soup.select('')
+    #     else:
+    #         authorIntro = soup.select('div[class="indent"] > div > div[class="intro"]')[0].get_text().strip()
+    #     result['作者简介']['intro'] = authorIntro
 
 
     # try:
@@ -230,9 +267,9 @@ def handleBook(text):
     return result
 
 def writeFile(fileName,dictData):
-    json_str = json.dumps(dictData)
-    with open('./docs/{}'.format(fileName),'w') as file:
-        file.write(json_str)
+    # json_str = json.dumps(dictData)
+    with open('./docs/{}'.format(fileName),'w',encoding='utf8') as file:
+        json.dump(dictData,file,ensure_ascii=False)
     file.close()
 
 
@@ -242,18 +279,37 @@ if __name__ == '__main__':
         "data":[]
     }
 
-    
-    writeFile('Book.json',movieResult)
-    # URLs = getMovieURL()  
-    # i=1
+    bookResult = {
+        "data":[]
+    }
+    # writeFile('Book.json',movieResult)
+
+    # URLs = getBookURL()
+    # i = 1
     # for url in URLs:
-    #     print('正在爬取',i)
+    #     print("正在爬取",i,' ',url)
     #     i += 1
-    #     responseText = doubanMovie('https://movie.douban.com/subject/1292220/')
-    #     result = handleMovieData(responseText)
-    #     movieResult['data'].append(result)
-    #     time.sleep(1)
-    # writeFile('Movie.json',movieResult)
+    #     responseText = doubanBook(url)
+    #     result = handleBook(responseText)
+    #     bookResult['data'].append(result)
+    # writeFile('Book.json',bookResult)
+
+
+
+    # text = doubanBook('https://book.douban.com/subject/1001885/')
+    # result = handleBook(text)
+    # print(result)
+
+    URLs = getMovieURL()  
+    i=1
+    for url in URLs:
+        print('正在爬取',i,' ',url)
+        i += 1
+        responseText = doubanMovie(url)
+        result = handleMovieData(responseText)
+        movieResult['data'].append(result)
+        # time.sleep(1)
+    writeFile('Movie.json',movieResult)
     
 
 
