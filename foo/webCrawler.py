5e8548bce84fb320e67c3547f9c42494099dd19f
Alcremie <2282931136@qq.com>
2022-11-11 11:09:49 +0800

    版本2

--- foo/webCrawler.py
+++ foo/webCrawler.py
@@ -1,10 +1,19 @@
 import requests
-
+import time
 
 urlMovie = 'https://movie.douban.com/subject/{}/'
 urlBook = 'https://book.douban.com/subject/{}/'
 
 def getMovieURL():
-    pass
+    # with open('1.txt','r') as movieFile:
+    #     print(movieFile.readline())
+    file = open('1.txt','r',encoding='utf-8')
+    print(file.readline())
+    file.close()
 def getBookURL():
     pass
+
+
+
+if __name__ == '__main__':
+    getMovieURL()
